fc0de9b710c3db54235164172c77778613c389d6
Senthil110499 <ssenthil8760650010@gmail.com>
2024-03-13 08:31:41 +0530

    Day 25-Task

---
+++ README.md
@@ -0,0 +1,44 @@
+
+## **_NodeJS File System Task_**
+
++ This repository is created for NodeJS File System.
+
++ This application creates a web server on port 4000 with two endpoints.
+
++ /: Provides instructions to visit /write endpoint to save the current Time_Stamp.
+
++ /write: Saves the current Time_Stamp to a file named dd-mm-yyyy-hh-mm-ss.txt.
+
++ The root endpoint generates a text file in a folder named Time_Stamp containing the current timestamp and sends its content as a response.
+ 
++ /read: Saves the current Time_Stamp to a file named dd-mm-yyyy-hh-mm-ss.txt.
+
++ The /getfiles endpoint retrieves and returns a list of all text files in the same folder.
+
+## **_Technology Used_**
+
++ Nodejs
++ Expressjs
+
+## **_Tools Used_**
+
++ Vs Code
++ Render
+
+Nodejs File System Task Render Link()
+
++ `http://localhost:4000`
+  
+![/](<Postman Images/localhost 4000.png>)
+
++ `http://localhost:4000/write `
+
+![/Write](<Postman Images/Write.png>)
+
++ `http://localhost:4000/read`
+
+![/Read](<Postman Images/Read.png>)
+
++ `http://localhost:4000/getfiles`
+
+![/getfiles](<Postman Images/getfiles.png>)
---
+++ index.js
@@ -0,0 +1,83 @@
+import express from "express";
+//import file system inbuilt module
+import fs from "fs";
+
+import { format } from "date-fns";
+import path from 'path';
+
+const app = express();
+//the server port 4000
+const PORT = 4000;
+
+// function to convert UTC time to Indian Standard Time (IST)
+function convertToIST(utc) {
+    return new Date(utc.toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
+}
+
+
+//the date  is formatted to be displayed as dd/MM/yyyy HH:mm:ss
+
+let today = convertToIST(new Date());
+let date = format(today, "dd-MM-yyyy HH-mm-ss");
+const filePath = `Time_Stamp/${date}.txt`;
+  
+app.get('/', (req, res) => {
+    res.status(200).send(` <h1>Welcome to Nodejs file system.<br/>  To retrive  Time_Stamp use the endpoint <mark>/getfiles</mark></h1>`);
+})
+app.get("/write", (req, res) => {
+    try {
+      //to write file
+    fs.writeFileSync(filePath, `${date}`, "utf8");
+    res
+      .status(200)
+      .send(
+        `<h1 style="text-align:center;background-color:green;">Time_Stamp : <br/><br/>${date}</h1>`
+      );
+  } catch (error) {
+    console.error("Error writing file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/read", (req, res) => {
+
+    try {
+      //to read file
+      // const filePath='Time_Stamp';
+       let data=fs.readFileSync(filePath, "utf8");
+      
+    res
+      .status(200)
+      .send(
+       `<h1 style="text-align:center;background-color:yellow;">${data}</h1>`
+        
+      );
+  } catch (error) {
+    console.error("Error reading file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// New endpoint to retrieve all text files in a folder
+
+app.get("/getfiles", (req, res) => {
+  const filePath = "Time_Stamp";
+
+  fs.readdir(filePath, (err, files) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .send("An error occurred while listing the files from the directory");
+    } else {
+      const textFiles = files.filter((file) => path.extname(file) === ".txt");
+      res.status(200).json(textFiles);
+    }
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on the port ${PORT}`);
+});
